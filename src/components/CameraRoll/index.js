a702a96fffa13af5f2d166f6f9f486a60bf19d1b
Mikhail Vasin <michaelvasin@gmail.com>
2018-04-22 22:08:32 +0300

    Save

--- src/components/CameraRoll/index.js
+++ src/components/CameraRoll/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './index.css';
 
-class CameraRoll extends React.Component {
-  render() {
-    const Images = this.props.images.map((image, index) => (
-      <img key={index} src={image} className="CameraRoll-Image" />
-    ));
-
-    return <div className="CameraRoll-Container">{Images}</div>;
-  }
-}
-
-export default CameraRoll;
+export default props => {
+  const itemHeight = 100;
+  return (
+    <div className="CameraRoll">
+      {props.images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={'portfolio #' + index}
+          className="CameraRoll-Image"
+          style={{ height: `${itemHeight}%` }}
+        />
+      ))}
+    </div>
+  );
+};
