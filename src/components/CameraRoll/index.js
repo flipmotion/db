8311ec45321ba74f2a8c30c08201f356b84e3747
Mikhail Vasin <michaelvasin@gmail.com>
2018-04-27 14:22:51 +0300

    Move CameraRoll to styled-components

--- src/components/CameraRoll/index.js
+++ src/components/CameraRoll/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './index.css';
+import styled from 'styled-components';
+
+const Roll = styled.div`
+  flex-direction: column;
+  /* margin-top: 100%; */
+  height: 100%;
+  /* transform: translateY(see index.js); */
+  transition: transform 1s ease 0s;
+`;
+
+const Image = styled.img`
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+  width: 100%;
+  /* height: see index.js; */
+  object-fit: cover;
+`;
 
 const CameraRoll = props => {
   const itemHeight = 40;
   const offsetToCenter = (100 - itemHeight) / 2;
   const offset = offsetToCenter - itemHeight * props.current;
   return (
-    <div className="CameraRoll" style={{ transform: `translateY(${offset}%)` }}>
+    <Roll style={{ transform: `translateY(${offset}%)` }}>
       {props.images.map((image, index) => (
         <Link key={index} to={`/portfolio#${index + 1}`}>
-          <img
+          <Image
             src={image}
             alt={'portfolio #' + index}
-            className="CameraRoll-Image"
             style={{ height: `${itemHeight}%` }}
             onDragStart={e => {
               e.preventDefault();
@@ -21,7 +36,7 @@ const CameraRoll = props => {
           />
         </Link>
       ))}
-    </div>
+    </Roll>
   );
 };
 
