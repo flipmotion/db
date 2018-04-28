4e397662c4f82fc64683c22143fbdc56c32916d2
Mikhail Vasin <michaelvasin@gmail.com>
2018-04-28 12:08:26 +0300

    Make small portfolio components private
    sort of :)

--- src/components/PortfolioPage/Portfolio.js
+++ src/components/PortfolioPage/Portfolio.js
@@ -34,6 +34,8 @@ const DescriptionArea = styled.div`
   align-self: center;
 `;
 
+// A dumb (presentational) component that combines and orchestrates smaller components
+// that are also dumb
 class Portfolio extends React.Component {
   render() {
     const {
@@ -61,41 +63,4 @@ class Portfolio extends React.Component {
   }
 }
 
-// This is a wrapper component that extracts pointer to current portfolio item from hash
-// and passes it to inner component
-// a number in hash starts from 1, the corresponding current starts from 0,
-// so /portfolio#1 means portfolio[0]
-const PortfolioPage = props => {
-  const { portfolio, animationStage } = props;
-  const titles = portfolio.map(el => el.title);
-  const images = portfolio.map(el => el.images[0]);
-  const descriptions = portfolio.map(el => el.description);
-
-  return (
-    <React.Fragment>
-      {/*       <Route exact path="/portfolio">
-        <Redirect to="/portfolio#1" />
-      </Route> */}
-
-      <Route
-        render={({ _, location }) => {
-          const currentFromHash = parseInt(location.hash.substring(1), 10) - 1;
-          // point to first element if hash is weird
-          let current =
-            isNaN(currentFromHash) || currentFromHash < 0 ? 0 : currentFromHash;
-          return (
-            <Portfolio
-              current={current}
-              titles={titles}
-              images={images}
-              descriptions={descriptions}
-              animationStage={animationStage}
-            />
-          );
-        }}
-      />
-    </React.Fragment>
-  );
-};
-
-export default PortfolioPage;
+export default Portfolio;
