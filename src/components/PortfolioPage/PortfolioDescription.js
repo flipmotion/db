7cef43b067f4dcef05cc91a6f34d2bbc4fd0039f
Mikhail Vasin <michaelvasin@gmail.com>
2018-04-28 17:31:18 +0300

    Animate description

--- src/components/PortfolioPage/PortfolioDescription.js
+++ src/components/PortfolioPage/PortfolioDescription.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const SpaceAround = styled.div`
-  padding: 1rem;
-`;
-
-class PortfolioDescription extends React.Component {
-  render() {
-    const description = this.props.descriptions[this.props.current];
-    return <SpaceAround>{description}</SpaceAround>;
-  }
+function style(animationStage) {
+  if (animationStage === 'entered')
+    return {
+      opacity: 1,
+      transition: 'opacity 1.5s 0.8s'
+    };
+  return {
+    opacity: 0,
+    transition: 'opacity 1s ease-out'
+  };
 }
 
-export default PortfolioDescription;
+export default ({ descriptions, current, animationStage }) => {
+  const description = descriptions[current];
+  return <div style={style(animationStage)}>{description}</div>;
+};
