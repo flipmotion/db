0b4a097effcb4d7b4e01082cd19644f1c34dbb50
Mikhail Vasin <michaelvasin@gmail.com>
2018-04-29 13:41:54 +0300

    Simulate hover with internal state!

--- src/components/Menu/Toggle/index.js
+++ src/components/Menu/Toggle/index.js
@@ -25,11 +25,6 @@ const Flag = styled.img`
   transition: all 0.5s;
   cursor: pointer;
   display: inline-block;
-  &:hover {
-    transform: scale(1.5) rotate3d(0, 1, 0, -30deg);
-    transition: all 0.5s;
-    filter: grayscale(10%) brightness(90%);
-  }
 `;
 
 export default class Toggle extends Component {
@@ -37,12 +32,36 @@ export default class Toggle extends Component {
     super();
     this.state = {
       langs: langs,
-      current: 0
+      current: 0,
+      rotation: 0,
+      scale: 1,
+      grayscale: 30,
+      brightness: 80
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.changeLang = this.changeLang.bind(this);
+    this.lift = this.lift.bind(this);
+    this.default = this.default.bind(this);
   }
 
-  handleClick() {
+  lift() {
+    this.setState({
+      rotation: -30,
+      scale: 1.5,
+      grayscale: 10,
+      brightness: 90
+    });
+  }
+
+  default() {
+    this.setState({
+      rotation: 0,
+      scale: 1,
+      grayscale: 30,
+      brightness: 80
+    });
+  }
+
+  changeLang() {
     const nextIndex = () => {
       const maxIndex = this.state.langs.length - 1;
       const current = this.state.current;
@@ -54,13 +73,22 @@ export default class Toggle extends Component {
   }
 
   render() {
+    const { rotation, scale, grayscale, brightness } = this.state;
+    const animatedStyle = {
+      transform: `scale(${scale}) rotate3d(0, 1, 0, ${rotation}deg)`,
+      filter: `grayscale(${grayscale}%) brightness(${brightness}%)`
+    };
+
     const { langs, current } = this.state;
     return (
       <Perspective>
         <Flag
           src={langs[current].img}
-          onClick={this.handleClick}
+          onClick={this.changeLang}
+          onMouseEnter={this.lift}
+          onMouseLeave={this.default}
           alt={langs[current].alt}
+          style={animatedStyle}
         />
       </Perspective>
     );
