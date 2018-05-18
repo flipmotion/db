32c81141be1f78d6f8ee1818cc9126f60a10a7ab
Mikhail Vasin <michaelvasin@gmail.com>
2018-05-18 22:07:45 +0300

    Translate home page!

--- src/components/HomePage.stories.js
+++ src/components/HomePage.stories.js
@@ -1,71 +1,65 @@
-// import React, { Component } from 'react';
-// import { storiesOf } from '@storybook/react';
-// import HomePage from './HomePage';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
-// import Menu from './Menu/Menu';
-// import MenuItem from './Menu/MenuItem';
-// import LangIcon from './Menu/LangIcon';
-// import contentIn from '../content'
+import React, { Component } from 'react';
+import { storiesOf } from '@storybook/react';
+import HomePage from './HomePage';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs/react';
+import Menu from './Menu/Menu';
+import MenuItem from './Menu/MenuItem';
+import LangIcon from './Menu/LangIcon';
+import contentIn from '../content';
 
-// const homePage = storiesOf('Home page', module);
+const homePage = storiesOf('Home page', module);
 
-// homePage.addDecorator(story => (
-//   <Router>
-//     <Menu
-//       links={contentIn('ru').menu.top.map((item, index) => (
-//         <MenuItem to={item.to} key={index}>
-//           {item.text}
-//         </MenuItem>
-//       ))}
-//     >
-//       {story()}
-//     </Menu>
-//   </Router>
-// ));
+// A minimal app that provides a lang and a function to change that lang
+// to render prop
+class IntApp extends Component {
+  constructor() {
+    super();
+    this.state = { lang: 'ru' };
+    this.toggleLang = this.toggleLang.bind(this);
+  }
 
-// homePage.addDecorator(withKnobs);
+  toggleLang() {
+    this.setState(
+      state => (state.lang === 'ru' ? { lang: 'en' } : { lang: 'ru' })
+    );
+  }
 
-// class IntApp extends Component {
-//   constructor() {
-//     super();
-//     this.state = {lang: 'ru'};
-//     this.toggleLang = this.toggleLang.bind(this);
-//   }
+  render() {
+    return this.props.render(this.state.lang, this.toggleLang);
+  }
+}
 
-//   toggleLang() {
-//     this.setState(state => state.lang === 'ru' ? {lang: 'en'} : {lang: 'ru'})
-//   }
-
-//   render() {
-//     return (
-//       <Menu
-//         logo={<Logo />}
-//         links={contentIn(this.state.lang).menu.top.map((item, index) => (
-//           <MenuItem key={index} to={item.to}>
-//             {item.text}
-//           </MenuItem>
-//         ))}
-//         icon={<LangIcon onClick={this.toggleLang} />}
-//         children={<Content />}
-//       />
-//     )
-//   }
-// }
-
-// menu.add('Translation', () => <IntApp />);
-
-// homePage.add('Home page', () => (
-//   <HomePage
-//     animationStage={boolean('visible', true) ? 'entered' : 'exited'}
-//     header={text('Header', globalState.content.homePage.header)}
-//     paragraphText={text(
-//       'Paragraph text',
-//       globalState.content.homePage.paragraphText
-//     )}
-//     linkText={text('Link text', globalState.content.homePage.link.text)}
-//     linkPath={globalState.content.homePage.link.path}
-//     imageSrc={globalState.content.homePage.media[0].src}
-//     imageAlt={globalState.content.homePage.media[0].alt}
-//   />
-// ));
+homePage.add('Menu', () => (
+  <IntApp
+    render={(lang, toggleLang) => {
+      const content = contentIn(lang);
+      return (
+        <Router>
+          <Menu
+            icon={<LangIcon onClick={toggleLang} />}
+            links={content.menu.top.map((item, index) => (
+              <MenuItem to={item.to} key={index}>
+                {item.text}
+              </MenuItem>
+            ))}
+            children={
+              <HomePage
+                animationStage={boolean('visible', true) ? 'entered' : 'exited'}
+                header={text('Header', content.homePage.header)}
+                paragraphText={text(
+                  'Paragraph text',
+                  content.homePage.paragraphText
+                )}
+                linkText={text('Link text', content.homePage.link.text)}
+                linkPath={content.homePage.link.path}
+                imageSrc={content.homePage.media[0].src}
+                imageAlt={content.homePage.media[0].alt}
+              />
+            }
+          />
+        </Router>
+      );
+    }}
+  />
+));
