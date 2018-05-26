5e7fc08ed6543777b9d24c128e920b33a36ec917
Mikhail Vasin <michaelvasin@gmail.com>
2018-05-26 19:47:37 +0300

    Improve app layout

--- src/App.js
+++ src/App.js
@@ -4,39 +4,53 @@ import MenuContainer from './containers/MenuContainer';
 import AppRouter from './containers/AppRouter';
 import IntApp from './containers/IntApp';
 import contentIn from './content';
-import { TopBar } from './components/Menu/MenuBars';
+import { TopBar, BottomBar } from './components/Menu/MenuBars';
 import LangIcon from './components/Menu/LangIcon';
 import LogoWrapper from './components/Menu/LogoWrapper';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    /* overflow: hidden; */
+    /* -webkit-overflow-scrolling: touch; */
+  }
+`;
 
 const App = props => (
   <IntApp
     render={(lang, toggleLang) => {
       const content = contentIn(lang);
       return (
-        <Router>
-          <MenuContainer
-            topBar={
-              <TopBar
-                logo={
-                  <LogoWrapper>
-                    <Link to="/">
-                      <img
-                        src={content.logo.src}
-                        alt={content.logo.alt}
-                        style={{ height: '3rem' }}
-                      />
-                    </Link>
-                  </LogoWrapper>
-                }
-              >
-                {content.menu.top}
-                <LangIcon onClick={toggleLang} lang={lang} />
-              </TopBar>
-            }
-          >
-            <AppRouter lang={lang} />
-          </MenuContainer>
-        </Router>
+        <div style={{ height: '100vh' }}>
+          <Router>
+            <MenuContainer
+              topBar={
+                <TopBar
+                  logo={
+                    <LogoWrapper>
+                      <Link to="/">
+                        <img
+                          src={content.logo.src}
+                          alt={content.logo.alt}
+                          style={{ height: '3rem' }}
+                        />
+                      </Link>
+                    </LogoWrapper>
+                  }
+                >
+                  {content.menu.top}
+                  <LangIcon onClick={toggleLang} lang={lang} />
+                </TopBar>
+              }
+              bottomBar={<BottomBar>{content.menu.bottom}</BottomBar>}
+            >
+              <AppRouter lang={lang} />
+            </MenuContainer>
+          </Router>
+        </div>
       );
     }}
   />
--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap-reboot.css';
-import { injectGlobal } from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
-
-injectGlobal`
-  body {
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    overflow: hidden;
-    /* -webkit-overflow-scrolling: touch; */
-  }
-`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
