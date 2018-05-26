1fa2c2340ce58a74dc60245d8a95f4af5156e6fb
Mikhail Vasin <michaelvasin@gmail.com>
2018-05-26 17:43:38 +0300

    Fix according to the new menu api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Menu from './components/Menu';
+import MenuContainer from './containers/MenuContainer';
 import AppRouter from './containers/AppRouter';
 import IntApp from './containers/IntApp';
 import contentIn from './content';
+import { TopBar } from './components/Menu/MenuBars';
+import LangIcon from './components/Menu/LangIcon';
+import LogoWrapper from './components/Menu/LogoWrapper';
 
 const App = props => (
   <IntApp
@@ -11,9 +14,22 @@ const App = props => (
       const content = contentIn(lang);
       return (
         <Router>
-          <Menu toggleLang={toggleLang} links={content.menu.top}>
+          <MenuContainer
+            topBar={
+              <TopBar
+                logo={
+                  <LogoWrapper>
+                    <p>DB</p>
+                  </LogoWrapper>
+                }
+              >
+                {content.menu.top}
+                <LangIcon onClick={toggleLang} />
+              </TopBar>
+            }
+          >
             <AppRouter lang={lang} />
-          </Menu>
+          </MenuContainer>
         </Router>
       );
     }}
