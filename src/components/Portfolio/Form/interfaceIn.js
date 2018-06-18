e9b270dadd2f0ad283cb16ef03ecda2574939df1
Mikhail Vasin <michaelvasin@gmail.com>
2018-06-18 17:31:58 +0200

    Change my i18n API

--- src/components/Portfolio/Form/interfaceIn.js
+++ src/components/Portfolio/Form/interfaceIn.js
@@ -5,12 +5,18 @@ const text = {
   }
 };
 
-// Use it as a closure, locking in the lang variable:
-// const phrase = (phraseId) => interfaceIn(lang, phraseId)
-// phrase('Enter something here')
-export default function interfaceIn(lang, phraseId) {
-  const phrase = text[phraseId];
-  if (!phrase) throw new Error(`Phrase id '${phraseId}' was not found`);
-  // Falls back
-  return phrase[lang] || phrase['ru'] || phrase[0];
+// This fn closes in interface from the text array above and
+// lang variable from properties to provide a function that just
+// serves desired phrases by phrase ids.
+// Use it like this:
+// const phrase = interfaceIn(props.lang)
+// and then...
+/// phrase('some phrase id')
+export default function interfaceIn(lang) {
+  return function(phraseId) {
+    const phrase = text[phraseId];
+    if (!phrase) throw new Error(`Phrase id '${phraseId}' was not found`);
+    // Falls back
+    return phrase[lang] || phrase['ru'] || phrase[0];
+  };
 }
