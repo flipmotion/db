669bd85260057fb8115d071a4490c321ea4770bc
Mikhail Vasin <michaelvasin@gmail.com>
2018-06-29 14:25:11 +0300

    Add feedback items list

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,4 +1,47 @@
 import React from 'react';
-// import styled from 'styled-components'
+import styled from 'styled-components';
+import { Vertical } from '../common';
+import contentIn from './content';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
+import uuid from 'uuid';
 
-export default () => <p>Feedback</p>;
+const interfaceText = {
+  'clients about us': {
+    ru: 'Клиенты о нас',
+    en: 'Clients about us'
+  }
+};
+
+const activeClassName = uuid();
+const StyledNavLink = styled(NavLink).attrs({ activeClassName })`
+  display: block;
+  padding-bottom: 1em;
+  text-decoration: none;
+  transition: padding 0.85s ease-out;
+
+  &.${activeClassName} {
+    padding-left: 2em;
+  }
+`;
+
+function interfaceIn(lang) {
+  return phraseId => interfaceText[phraseId][lang];
+}
+
+function Feedback({ lang }) {
+  const cases = contentIn(lang);
+  return (
+    <Router>
+      <div>
+        <h1>{interfaceIn(lang)('clients about us')}</h1>
+        {cases.map(c => (
+          <StyledNavLink key={c.path} to={`/feedback/${c.path}`}>
+            {c.title}
+          </StyledNavLink>
+        ))}
+      </div>
+    </Router>
+  );
+}
+
+export default Feedback;
